2b2bb4f6cda4fc0f901e4a4bc8d0534788660b28
JuanCaArredono <juan.arredondo@diverza.com>
2022-07-25 23:28:33 +0000

    se agregan fucniones para el trade

--- config/secrets.ex
+++ config/secrets.ex
@@ -2,5 +2,5 @@
 import Config
 
 config :binance,
-  api_key: "YOUR-API-KEY-HERE",
-  secret_key: "YOUR-SECRET-KEY-HERE"
+  api_key: "v3nUqULmLK8V6bg1GcWL7mJzEJ9QmSXL0TiQDJwGdRNY7w3ToPNhepJAftpgneX4",
+  secret_key: "BnYSFtUCVRezoMRDPitMcqr6jhL5OJezQfVNV8x8dy6TfV767ltDOagQ1d5iWuEO"
