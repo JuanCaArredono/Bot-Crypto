7ec0fb746064c05e5f62fce3f6172018d480e7fd
JuanCaArredono <juan.arredondo@diverza.com>
2022-07-25 23:31:23 +0000

    se modifica git ignore

--- config/secrets.ex
+++
@@ -1,6 +0,0 @@
-# /config/secrets.exs
-import Config
-
-config :binance,
-  api_key: "v3nUqULmLK8V6bg1GcWL7mJzEJ9QmSXL0TiQDJwGdRNY7w3ToPNhepJAftpgneX4",
-  secret_key: "BnYSFtUCVRezoMRDPitMcqr6jhL5OJezQfVNV8x8dy6TfV767ltDOagQ1d5iWuEO"
