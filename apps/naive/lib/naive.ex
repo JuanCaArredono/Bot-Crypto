d20e2229db3d37e82a116e2f0c9a5c7c973ae664
JuanCaArredono <juan.arredondo@diverza.com>
2022-07-27 17:16:55 +0000

    Se implementa PubSub para la comunicacion

--- apps/naive/lib/naive.ex
+++ apps/naive/lib/naive.ex
@@ -4,7 +4,4 @@ defmodule Naive do
   """
   alias Streamer.Binance.TradeEvent
 
-  def send_event(%TradeEvent{} = event) do
-    GenServer.cast(:trader, event)
-  end
 end
--- apps/naive/lib/naive/trader.ex
+++ apps/naive/lib/naive/trader.ex
@@ -24,6 +24,10 @@ defmodule Naive.Trader do
 
     Logger.info("Initializing new trader for #{symbol}")
 
+    Phoenix.PubSub.subscribe(
+      Streamer.PubSub,
+      "TRADE_EVENTS:#{symbol}"
+    )
     tick_size = fetch_tick_size(symbol)
 
     {:ok,
@@ -34,7 +38,7 @@ defmodule Naive.Trader do
      }}
   end
 
-  def handle_cast(
+  def handle_info(
     %TradeEvent{price: price},
     %State{symbol: symbol, buy_order: nil} = state
   ) do
@@ -48,7 +52,7 @@ defmodule Naive.Trader do
     {:noreply, %{state | buy_order: order}}
   end
 
-  def handle_cast(
+  def handle_info(
     %TradeEvent{
       buyer_order_id: order_id,
       quantity: quantity
@@ -77,7 +81,7 @@ defmodule Naive.Trader do
     {:noreply, %{state | sell_order: order}}
   end
 
-  def handle_cast(
+  def handle_info(
     %TradeEvent{
       seller_order_id: order_id,
       quantity: quantity
@@ -93,7 +97,7 @@ defmodule Naive.Trader do
     {:stop, :normal, state}
   end
 
-  def handle_cast(%TradeEvent{}, state) do
+  def handle_info(%TradeEvent{}, state) do
     {:noreply, state}
   end
 
